13ad8a2942dc8e84d5fe68f81efd7e0519d12bdc
Arif <arahman.isrt@gmail.com>
2025-03-13 23:00:35 -0500

    update

--- app/services/article_generator_service.py
+++ app/services/article_generator_service.py
@@ -1,6 +1,7 @@
 from app.services.openai_service import generate_article
 from app.database.article_repository import ArticleRepository
 from datetime import datetime, timezone
+from app.config import config
 
 class ArticleGeneratorService:
     """Handles AI-based article generation and saving to the database."""
@@ -11,16 +12,16 @@ class ArticleGeneratorService:
         now = datetime.now(timezone.utc).isoformat()
         new_articles = []
 
-        for sector in ["Tech", "Health", "Corporate", "Politics", "Youth", "Lifestyle"]:
+        for sector in config.SECTORS:
             content = await generate_article(sector)
             print('Generated content: ', content)
 
             article_data = {
                 "title": content.split("\n\n")[0].strip("*"),
                 "description": "\n\n".join(content.split("\n\n")[1:]),  # Content becomes 'description'
-                "tags": [sector],  # Changed 'sector' to a list of tags
+                "tags": [sector], 
                 "img": f"https://picsum.photos/800/450?{sector}",  # Added a default image
-                "authors": ['AI'],  # Placeholder for authors
+                "authors": ['AI'],
                 "created_at": now,
                 "updated_at": now
             }
